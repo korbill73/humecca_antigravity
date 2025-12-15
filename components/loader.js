edd86ae52ba0e57d69c673abe45f26198ab18d4d
박제군 <korbill73@gmail.com>
2025-12-15 15:11:05 +0900

    test: update version to v.20251215.1509 for develop preview

--- components/loader.js
+++ components/loader.js
@@ -5,7 +5,7 @@
 
 // [Configuration] Current Application Version
 // Update this value manually whenever a deployment/update occurs
-const APP_VERSION = 'v.20251215.1324';
+const APP_VERSION = 'v.20251215.1509';
 
 
 
