3fbf3a5ac7a26f411660f1a63700d6194d8839ac
박제군 <korbill73@gmail.com>
2025-12-28 22:46:24 +0900

    Fix: Add missing TCO infographic and content (Force Redeploy v.20251228.2030)

--- components/loader.js
+++ components/loader.js
@@ -5,7 +5,7 @@
 
 // [Configuration] Current Application Version
 // Update this value manually whenever a deployment/update occurs
-const APP_VERSION = 'v.20251228.1300';
+const APP_VERSION = 'v.20251228.1945';
 
 
 
